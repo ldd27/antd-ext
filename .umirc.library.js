884d3d4c2117aae521a42e335fc7be448a29d85a
liaodd <liaodd@codoon.com>
2019-03-19 14:38:19 +0800

    add 按需加载

--- .umirc.library.js
+++ .umirc.library.js
@@ -4,5 +4,12 @@ export default {
   cjs: 'rollup',
   doc: {
     base: '/antd-ext'
-  }
+  },
+  extraBabelPlugins: [
+    ['babel-plugin-import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }],
+  ],
 }
